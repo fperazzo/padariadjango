ff2ee8f66d154296da2999af29a1e83393dd0c21
unknown <netovoski@gmail.com>
2020-09-15 22:57:48 -0300

    heroku

--- projpad/settings.py
+++ projpad/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -130,3 +132,5 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CKEDITOR_UPLOAD_PATH = 'upload/'
+
+django_heroku.settings(locals())
